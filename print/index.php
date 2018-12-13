f69e738c0d2382f61fbc1ba2a1adeaec0ca70ac6
krkonsap <kristjan.konsap@khk.ee>
2018-12-13 13:06:50 +0200

    teksti väljastamine

--- print/index.php
+++ print/index.php
@@ -5,10 +5,15 @@
  * Date: 13.12.2018
  * Time: 12:46
  */
+
+
+// teksti väljastamine
 print('Tere maailm!<br>');
 print('PHP Alused <br>');
 
-echo 'Nii see käibki';
+echo 'Nii see käibki <br>';
+//väärtuste ühendamine .
+echo 'See '.'on '.'kolmest '.'kokku '.'punktiga (.).<br>';
 
 
 ?>
