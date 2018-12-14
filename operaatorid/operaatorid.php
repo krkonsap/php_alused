0eaef3d77d2d89de3931a066e5f4055eb7019695
krkonsap <kristjan.konsap@khk.ee>
2018-12-14 12:21:07 +0200

    mata

---
+++ operaatorid/operaatorid.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kristjan.konsap
+ * Date: 14.12.2018
+ * Time: 12:13
+ */
+
+// matemaatilised ja võrdlusoperaatorid
+
+$arv1 = 42;
+$arv2 = 11;
+
+echo $arv1.' + '.$arv2.' = '.($arv1 + $arv2).'<br>';
