8cb37d206e6d32e59ca94d664276928dbb4735ac
krkonsap <kristjan.konsap@khk.ee>
2018-12-18 14:49:27 +0200

    andmemassiivi loomine

---
+++ arrays/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kristjan.konsap
+ * Date: 18.12.2018
+ * Time: 14:42
+ */
+// indekseeritud lihtmassiivid - nö nimekirjad
+
+$kasutajad = array(
+    'alice',
+    'bob',
+    'lucy'
+);
