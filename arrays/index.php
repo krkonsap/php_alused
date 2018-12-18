edd62bc1d25e86115275073c94e98f1926d6c1bd
krkonsap <kristjan.konsap@khk.ee>
2018-12-18 15:08:50 +0200

    andmemassiivi andmete lisamine ja massiivide andmete kuvamine

--- arrays/index.php
+++ arrays/index.php
@@ -22,3 +22,35 @@ echo '<hr>';
 foreach ($kasutajad as $kasutaja){
     echo $kasutaja.'<br>';
 }
+
+echo '<hr>';
+// tühi array, kuhu hiljem nadmeid sisestada
+$kasutajad2 = array();
+
+$kasutajad2[] = 'alice';
+$kasutajad2[] = 'bob';
+
+array_push($kasutajad2, 'lucy');
+
+foreach ($kasutajad2 as $kasutaja2){
+    echo $kasutaja2.'<br>';
+}
+
+//eemaldamine massiivi tagant ja eest
+echo '<hr>';
+array_pop($kasutajad2);
+array_shift($kasutajad2);
+foreach ($kasutajad2 as $kasutaja2){
+    echo $kasutaja2.'<br>';
+}
+
+echo '<hr>';
+//andmete vaatamine
+var_dump($kasutajad);
+echo '<br>';
+print_r($kasutajad);
+echo '<pre>';
+print_r($kasutajad);
+echo '</pre>';
+
+echo '<hr>';
