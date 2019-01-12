d6e86037c6283f36da4003cd3f4a5806c435042e
krkonsap <kristjan.konsap@khk.ee>
2019-01-12 18:59:23 +0200

    lisatud ül 8 h 1

---
+++ date/UL8.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Laptop
+ * Date: 12.01.2019
+ * Time: 18:43
+ */
+date_default_timezone_set('Europe/Tallinn');
+
+// Kuva kuupäev ja kellaaeg formaadis 20.03.2013 12:31
+echo date('d.m.Y G:i' , time());
+echo '<hr>';
+//Kuva tänane eestikeelne nädalapäev, N: kolmapäev
+
