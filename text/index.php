6c550e2956366beb77c35d4cbce30e07f10022a2
krkonsap <kristjan.konsap@khk.ee>
2019-01-12 21:16:21 +0200

    lisatud suur-ja väiketähe funktsioonid

---
+++ text/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Laptop
+ * Date: 12.01.2019
+ * Time: 21:07
+ */
+//teksti vormindamine
+
+$tekst = 'see on mingi tekst';
+
+echo strtolower($tekst);
+echo '<br>';
+echo strtoupper($tekst);
+echo '<br>';
+echo ucfirst(strtolower($tekst));
