72e4675fd64b6936934c3d27c37460cedde42351
krkonsap <kristjan.konsap@khk.ee>
2019-01-12 18:12:10 +0200

    lisatud kera ruumala arvutamise funktsioon

--- form/UL3.php
+++ form/UL3.php
@@ -11,5 +11,26 @@ $h = $_GET['h'];
 
 echo 'Raadius: '.$r.' m<br>';
 echo 'Kõrgus: '.$h.' m<br>';
+
+$pii = 3.1416;
+
+//kera ruumala = 4/3 * pii * r**3
+
+function kera(){
+    global $r;
+    global $pii;
+    $keraV = 4 / 3 * $pii * ($r ** 3);
+    return $keraV;
+}
+echo 'Kera ruumala on '.kera().' kuupmeetrit.';
+
+//koonuse ruumala = pii * r**2 * (h/3)
+
+
+
+//silindri ruumala = pii * r**2 * h
+
+
+
 ?>
 
