426748eee36b0b71e936eae73fd84d36cd027786
krkonsap <kristjan.konsap@khk.ee>
2019-01-12 20:39:14 +0200

    lisatud nädalapäeva kuvamine

--- date/UL8.php
+++ date/UL8.php
@@ -10,5 +10,11 @@ date_default_timezone_set('Europe/Tallinn');
 // Kuva kuupäev ja kellaaeg formaadis 20.03.2013 12:31
 echo date('d.m.Y G:i' , time());
 echo '<hr>';
+
 //Kuva tänane eestikeelne nädalapäev, N: kolmapäev
 
+$date = date('d.m.Y G:i' , time());
+$days = ['esmaspäev', 'teisipäev', 'kolmapäev', 'neljapäev', 'reede','laupäev', 'pühapäev'];
+$day = date('w', strtotime($date));
+$date = 'Täna on'." $days[$day].";
+echo $date;
