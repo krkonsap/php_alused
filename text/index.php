5a1933f75f42eaee7359bf95a90a81a543e60a76
krkonsap <kristjan.konsap@khk.ee>
2019-01-12 21:18:04 +0200

    lisatud teksti pikkuse funktsioonid

--- text/index.php
+++ text/index.php
@@ -14,3 +14,12 @@ echo '<br>';
 echo strtoupper($tekst);
 echo '<br>';
 echo ucfirst(strtolower($tekst));
+
+echo '<hr>';
+//teksti pikkus
+echo strlen($tekst);
+echo '<br>';
+echo str_word_count($tekst);
+
+echo '<hr>';
+//
