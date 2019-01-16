ab5bfde5deb935f78e42927e4544b3900050419b
krkonsap <kristjan.konsap@khk.ee>
2019-01-16 15:51:52 +0200

    koodi taaskasutus - menu loomine

---
+++ recode/index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: konsap
+ * Date: 16.01.2019
+ * Time: 15:44
+ */
+
+?>
+
+<!doctype html>
+<html>
+<head>
+<title>Koodi taaskasutamine</title>
+</head>
+<body>
+    <menu>
+        <a href="index.php">Avaleht<a/>
+        <a href="index.php?leht=portfoolio">Portfoolio<a/>
+        <a href="index.php?leht=kaart">Kaart<a/>
+        <a href="index.php?leht=kontakt">Kontakt<a/>
+    </menu>
+</body>
+</html>
