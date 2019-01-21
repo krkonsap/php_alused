256d325681cd8dc996c469f790b5058524a0c553
kristjan.konsap <kristjan.konsap@khk.ee>
2019-01-21 13:15:56 +0200

    lisatud portfooliosse canvas

--- recode/portfoolio.php
+++ recode/portfoolio.php
@@ -1,6 +1,9 @@
 <h2>Portfoolio</h2>
 <p>Siin on näidatud minu tööd.</p>
 
+<canvas id="minuCanvas" width="200" height="200" style="border:1px solid #000;">
+    Su lehitseja ei toimi.
+</canvas>
 <?php
 /**
  * Created by PhpStorm.
