8845b788f042bcb24669f58d1b3ed0c43d08f35a
kristjan.konsap <kristjan.konsap@khk.ee>
2019-01-21 13:10:12 +0200

    lisatud kaart

--- recode/kaart.php
+++ recode/kaart.php
@@ -1,6 +1,6 @@
 <h2>Kaart</h2>
 <p>Väike topograafianurk.</p>
-
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4979.469626069062!2d26.709976327983604!3d58.349396267147625!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46eb37487c7817f9%3A0x31f91313d1bbe0ee!2sTartu+Vocational+Education+Center!5e0!3m2!1sen!2see!4v1548068973141" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 <?php
 /**
  * Created by PhpStorm.
