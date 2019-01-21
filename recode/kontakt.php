ee075651eba54555ae5558ce9b6c6e5dddd48481
kristjan.konsap <kristjan.konsap@khk.ee>
2019-01-21 13:13:26 +0200

    lisatud tel-nr ja ikoon

--- recode/kontakt.php
+++ recode/kontakt.php
@@ -1,6 +1,7 @@
 <h2>Kontakt</h2>
 <p>Helista mulle lauatelefonile.</p>
-
+<img src="http://www.stickpng.com/assets/thumbs/5a4525b2546ddca7e1fcbc82.png" width="100" height="100"/>
+<h4>+3727407048</h4>
 <?php
 /**
  * Created by PhpStorm.
