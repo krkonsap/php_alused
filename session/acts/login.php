fe1343b272e016ca22bde47809c818794e63a0d9
kristjan.konsap <kristjan.konsap@khk.ee>
2019-01-24 15:25:27 +0200

    session - login võimalus ja logout võimalus

--- session/acts/login.php
+++ session/acts/login.php
@@ -5,3 +5,28 @@
  * Date: 24.01.2019
  * Time: 14:34
  */
+
+require_once '../db/conf.php';
+require_once '../db/dbfnk.php';
+
+//andmed html vormist
+$username = $_GET['uname'];
+$password = $_GET['upass'];
+
+//loome ühenduse db-ga
+$ikt = connect_db(DBHOST,DBUSER,DBPASS,DBNAME);
+
+//küsime kasutaja olemasolu andmebaasist
+$sql = 'SELECT * FROM user WHERE username="'.$username.'" AND password="'.md5($password).'"';
+
+$users = getData($sql,$ikt);
+
+//loome kasutaja session kui ta andmebaasis olemas
+if($users !== false){
+    session_start();
+    $_SESSION['user'] = $users[0];
+    header('Location: ../index.php');
+//    echo session_id();
+//    echo $users[0]['first_name'].' '.$users[0]['last_name'];
+}
+
--- session/index.php
+++ session/index.php
@@ -5,7 +5,14 @@
  * Date: 24.01.2019
  * Time: 14:16
  */
+session_start();
 require_once 'fnk.php';
+
+if(isset($_SESSION['user'])){
+    echo 'Tere tulemast, '.$_SESSION['user']['last_name'].'!<br>';
+    echo '<form action="acts/login.php"><input type="submit" value="Logi välja"></form>';
+} else{
 
 $loginForm = htmlFromFile('login');
 echo $loginForm;
+}
