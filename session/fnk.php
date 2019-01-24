f6f97bf877fcbf30b9129db2f7ceece329fe2f72
kristjan.konsap <kristjan.konsap@khk.ee>
2019-01-24 14:32:08 +0200

    session - loodud funktsioon vormi kuvamiseks

--- session/fnk.php
+++ session/fnk.php
@@ -5,3 +5,11 @@
  * Date: 24.01.2019
  * Time: 14:17
  */
+
+function htmlFromFile($fileName){
+    $fileName = 'html/'.$fileName.'.html';
+    $fp = fopen($fileName, 'r');
+    $content = fread($fp, filesize($fileName));
+    fclose($fp);
+    return $content;
+}
--- session/index.php
+++ session/index.php
@@ -5,3 +5,7 @@
  * Date: 24.01.2019
  * Time: 14:16
  */
+require_once 'fnk.php';
+
+$loginForm = htmlFromFile('login');
+echo $loginForm;
